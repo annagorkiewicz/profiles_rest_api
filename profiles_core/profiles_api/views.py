66ecc3995f625792d591245e163a266dca495b9c
Anna Gorkiewicz <an.gorkiewicz@gmail.com>
2023-03-19 18:48:57 +0100

    add serializer and post method to the API View

--- profiles_core/profiles_api/views.py
+++ profiles_core/profiles_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 
 class HelloAPIView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns list of APIView features"""
@@ -17,3 +21,17 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_piview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello, {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
