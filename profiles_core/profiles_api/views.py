81ace4a4989e626428d864fcabced3981273dc91
Anna Gorkiewicz <an.gorkiewicz@gmail.com>
2023-03-20 13:13:02 +0100

    add new viewset and add url to the feeed

--- profiles_core/profiles_api/views.py
+++ profiles_core/profiles_api/views.py
@@ -102,14 +102,25 @@ class HelloViewSet(viewsets.ViewSet):
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
-    serializer_class =  serializers.UserProfileSerializer
+    serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
-    filter_backends = (filters.SearchFilter, )
+    filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
 
 
 class UserLoginAPIView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating Profile Feed Items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileItemFeedSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
